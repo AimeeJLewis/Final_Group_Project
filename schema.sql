3533b23663e6ca910b8ce1e36f78be48dd308b91
drosenquist <101379969+drosenquist@users.noreply.github.com>
2022-08-20 10:48:44 -0700

    Updating schema file

--- schema.sql
+++ schema.sql
@@ -1,113 +1,78 @@
--- Creating tables for brain_stroke
-CREATE TABLE health_data (
-	id VARCHAR(5) NOT NULL,
-	gender varchar NOT NULL,
+--Creating a demographics table for Brain-StrokeDB
+CREATE TABLE demographics (
+	id VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
 	age INT NOT NULL,
-	hypertension INT NOT NULL,
-	heart_disease INT NOT NULL,
-	ever_married varchar NOT NULL,
+	ever_married VARCHAR NOT NULL,
 	work_type VARCHAR NOT NULL,
-	Residence_type varchar NOT NULL,
-	avg_glucose_level FLOAT NOT NULL,
-	bmi FLOAT NOT NULL,
+	Residence_type VARCHAR NOT NULL,
 	smoking_status VARCHAR NOT NULL,
-	stroke INT NOT NULL,
-     PRIMARY KEY (id)
+	PRIMARY KEY (id)
 );
 
+--Creating health data table for Brain-StrokeDB
+CREATE TABLE health_data (
+	id VARCHAR NOT NULL,
+	hypertension INT NOT NULL,
+	heart_disease INT NOT NULL,
+	avg_glucose_level FLOAT NOT NULL,
+	bmi FLOAT NOT NULL,
+	stroke INT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+--Creating a gender table for Brain-StrokeDB
 CREATE TABLE gender (
-	id VARCHAR(5) NOT NULL,
-	gender varchar NOT NULL,
-     PRIMARY KEY (id)
+	id VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	stroke INT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE ever_married (
+	id VARCHAR NOT NULL,
+	ever_married VARCHAR NOT NULL,
+	stroke INT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE work_type (
+	id VARCHAR NOT NULL,
+	work_type VARCHAR NOT NULL,
+	stroke INT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Residence_type (
+	id VARCHAR NOT NULL,
+	Residence_type VARCHAR NOT NULL,
+	stroke INT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE smoking_status (
+	id VARCHAR NOT NULL,
+	smoking_status VARCHAR NOT NULL,
+	stroke INT NOT NULL,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE age (
-	id VARCHAR(5) NOT NULL,
+	id VARCHAR NOT NULL,
 	age INT NOT NULL,
-     PRIMARY KEY (id)
-);
-
-CREATE TABLE hypertension (
-	id VARCHAR(5) NOT NULL,
-	hypertension INT NOT NULL,
-     PRIMARY KEY (id)
-);
-
-
-CREATE TABLE heart_disease (
-	id VARCHAR(5) NOT NULL,
-	heart_disease INT NOT NULL,
-     PRIMARY KEY (id)
-);
-
-CREATE TABLE married (
-	id VARCHAR(5) NOT NULL,
-	ever_married varchar NOT NULL,
-     PRIMARY KEY (id)
-);
-
-CREATE TABLE work (
-	id VARCHAR(5) NOT NULL,
-	work_type VARCHAR NOT NULL,
-     PRIMARY KEY (id)
-);
-
-CREATE TABLE residence (
-	id VARCHAR(5) NOT NULL,
-	Residence_type varchar NOT NULL,
-     PRIMARY KEY (id)
-);
-
-CREATE TABLE glucose (
-	id VARCHAR(5) NOT NULL,
-	avg_glucose_level FLOAT NOT NULL,
-     PRIMARY KEY (id)
-);
-
-CREATE TABLE bmi (
-	id VARCHAR(5) NOT NULL,
-	bmi FLOAT NOT NULL,
-     PRIMARY KEY (id)
-);
-
-CREATE TABLE smoking (
-	id VARCHAR(5) NOT NULL,
-	smoking_status VARCHAR NOT NULL,
-     PRIMARY KEY (id)
-);
-
-CREATE TABLE stroke (
-	id VARCHAR(5) NOT NULL,
 	stroke INT NOT NULL,
-     PRIMARY KEY (id)
+	PRIMARY KEY (id)
 );
 
-ALTER TABLE glucose
-ADD stroke INT NOT NULL;
+SELECT * FROM demographics;
+SELECT * FROM health_data;
 
-ALTER TABLE bmi
-ADD stroke INT NOT NULL;
-
-ALTER TABLE work
-ADD stroke INT NOT NULL;
-
-ALTER TABLE married
-ADD stroke INT NOT NULL;
-
-ALTER TABLE residence
-ADD stroke INT NOT NULL;
-
-ALTER TABLE smoking
-ADD stroke INT NOT NULL;
-
-ALTER TABLE heart_disease
-ADD stroke INT NOT NULL;
-
-ALTER TABLE hypertension
-ADD stroke INT NOT NULL;
-
-ALTER TABLE age
-ADD stroke INT NOT NULL;
-
-ALTER TABLE gender
-ADD stroke INT NOT NULL;
+--Joining demographics and health_data tables
+SELECT health_data.hypertension,
+	health_data.heart_disease,
+	health_data.avg_glucose_level,
+	health_data.bmi,
+	health_data.stroke
+FROM health_data
+INNER JOIN demographics
+ON health_data.id = demographics.id;
